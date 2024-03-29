e96005ea9f13eb401c80ceae8d135c2a54944c35
rashmipullur <mheresince1998@gmail.com>
2024-03-30 00:10:54 +0530

    CitiesContext

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,11 +2,16 @@ import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
-  if (!cities.length) return <Message message="Add your first city by clicking on a city on the map"/>
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
 
   return (
     <ul className={styles.cityList}>
